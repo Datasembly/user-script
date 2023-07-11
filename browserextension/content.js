2ba6a0a7c26ddd735df7f2c1a64654a544ea8b78
Andrewferg <55989952+Andrewferg@users.noreply.github.com>
2023-07-11 15:50:21 -0300

    version num update

--- browserextension/content.js
+++ browserextension/content.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Datasembly UPC tools
 // @namespace    https://datasembly.com
-// @version      0.1.19
+// @version      0.1.2
 // @description  Help identify UPCs and product IDs
 // @author       Datsembly, Inc.
 // @match        *://*.walmart.com/*
